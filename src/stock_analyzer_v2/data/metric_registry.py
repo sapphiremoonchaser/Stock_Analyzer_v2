4692881e36e0d22c845c2c69bcfd94e07c90af61
violetdiamonds <violetdiamonds@protonmail.com>
2026-01-28 16:13:13 -0500

    adding metric registry

---
+++ src/stock_analyzer_v2/data/metric_registry.py
@@ -0,0 +1,24 @@
+from stock_analyzer_v2.data.enums.metrics import StockMetric
+from stock_analyzer_v2.data.enums.metric_category import MetricCategory
+from stock_analyzer_v2.data.models.metric import MetricDefinition
+
+
+MetricDefinition: dict[StockMetric, MetricDefinition] = {
+    StockMetric.pe_ratio: MetricDefinition(
+        metric=StockMetric.pe_ratio,
+        category=MetricCategory.valuation,
+        higher_is_better=False
+    ),
+
+    StockMetric.ev_to_ebitda: MetricDefinition(
+        metric=StockMetric.ev_to_ebitda,
+        category=MetricCategory.valuation,
+        higher_is_better=False
+    ),
+
+    StockMetric.free_cash_flow: MetricDefinition(
+        metric=StockMetric.free_cash_flow,
+        category=MetricCategory.cash_flow,
+        higher_is_better=False
+    )
+}
