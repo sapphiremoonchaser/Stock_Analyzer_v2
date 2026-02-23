e64c7c38e4eedb0b0105dc875d8efe4166c56e0c
violetdiamonds <violetdiamonds@protonmail.com>
2026-02-23 14:34:54 -0500

    adding docstrings

--- src/stock_analyzer_v2/data/enums/metrics.py
+++ src/stock_analyzer_v2/data/enums/metrics.py
@@ -1,9 +1,13 @@
-"""Enum for StockMetric
+"""
+Defines the StockMetric enumeration used throughout the stock analyzer project.
+
+StockMetric provides a standardized set of metrics. Update this when adding new metrics.
 """
 from enum import Enum
 
 
 class StockMetric(Enum):
+    """Enumeration for metrics used in the stock analyzer."""
     pe_ratio = 'pe_ratio'
     ev_to_ebitda = 'ev_to_ebitda'
     free_cash_flow = 'free_cash_flow'
