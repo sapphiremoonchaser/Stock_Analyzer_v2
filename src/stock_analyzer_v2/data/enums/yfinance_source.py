114c222835d3f3c84e62b2c9f92a736de2ba934f
violetdiamonds <violetdiamonds@protonmail.com>
2026-02-23 14:37:04 -0500

    adding docstrings

--- src/stock_analyzer_v2/data/enums/yfinance_source.py
+++ src/stock_analyzer_v2/data/enums/yfinance_source.py
@@ -1,7 +1,15 @@
+"""
+Defines the YFinanceSource enumeration used throughout the stock analyzer project.
+
+YFinanceSource provides a standardized set of yfiance data sources. For example,
+'sector' would come from 'info' whereas 'operating_cash_flow' would come from
+yfinance's 'cashflow' object.
+"""
 from enum import Enum
 
 
 class YFinanceSource(str, Enum):
+    """Enumeration for YFinanceSource used in the stock analyzer."""
     info = "info"
     financials = "financials"
     cash_flow = "cash_flow"
