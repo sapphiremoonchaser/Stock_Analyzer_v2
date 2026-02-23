779314e156be15fd369e31e1a0a86c7aeaeb359c
violetdiamonds <violetdiamonds@protonmail.com>
2026-02-23 14:32:44 -0500

    adding docstrings

--- src/stock_analyzer_v2/core/metric_availability_checker.py
+++ src/stock_analyzer_v2/core/metric_availability_checker.py
@@ -1,9 +1,21 @@
+"""
+Provides functionality for validating metric availability.
+
+This module defines the MetricAvailabilityChecker class, which determines whether the
+requested metric is available given a stock by attempting to retrieve via yfinance.
+
+The checker separates data retrieval (handed by the fetcher) and availability
+validation (handled here).
+"""
 from stock_analyzer_v2.core.yfinance_fetcher import YFinanceFetcher
 from stock_analyzer_v2.data.enums.metrics import StockMetric
 from stock_analyzer_v2.core.models.metric_availability import MetricAvailability
 
 
 class MetricAvailabilityChecker:
+    """
+    Evaluates whether or not requested stock metrics are available. It queries yfinance
+    """
     def __init__(
         self,
         fetcher: YFinanceFetcher
--- src/stock_analyzer_v2/data/enums/metric_category.py
+++ src/stock_analyzer_v2/data/enums/metric_category.py
@@ -1,7 +1,15 @@
+"""
+Defines the MetricCategory enumeration used throughout the stock analyzer project.
+
+MetricCategory provides a standardized set of categories of metrics. For example,
+pe_ratio would belong to the 'valuation' MetricCategory. Currently, it is only being
+used to categorize metrics.
+"""
 from enum import Enum
 
 
 class MetricCategory(str, Enum):
+    """Enumeration of supported metric categories."""
     valuation = 'valuation'
     cash_flow = 'cash_flow'
     growth = 'growth'
