46b8b4e7468a91f385bf45bc4f5ba651b257903c
violetdiamonds <violetdiamonds@protonmail.com>
2026-02-23 16:22:18 -0500

    pe_ratio metric definition test

--- tests/data/test_metric_definition.py
+++ tests/data/test_metric_definition.py
@@ -3,6 +3,23 @@ from stock_analyzer_v2.data.enums.metrics import StockMetric
 from stock_analyzer_v2.data.enums.metric_category import MetricCategory
 
 
+def test_pe_ratio_definition_metadata():
+    pe_ratio = METRICS[StockMetric.pe_ratio]
+
+    # Correct pe_ratio category and attributes
+    assert pe_ratio.category == MetricCategory.valuation
+    assert pe_ratio.higher_is_better is False
+    assert pe_ratio.is_ratio is True
+
+
+def test_ev_to_ebitda_definition_metadata():
+    pass
+
+
+def test_revenue_definition_metadata():
+    pass
+
+
 def test_metric_definition_metadata():
     fcf = METRICS[StockMetric.free_cash_flow]
 
