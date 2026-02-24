39d365ac4c5720e0dbb61bd4f582fe69db28a93f
violetdiamonds <violetdiamonds@protonmail.com>
2026-02-24 15:48:22 -0500

    adding example for fetching metrics

---
+++ examples/example_1_fetch_data_for_ticker.py
@@ -0,0 +1,19 @@
+from stock_analyzer_v2.core.yfinance_fetcher import YFinanceFetcher
+from stock_analyzer_v2.data.enums.metrics import StockMetric
+
+ticker_symbol = 'AAPL'
+fetcher = YFinanceFetcher(ticker_symbol)
+
+# Fetch pe_ratio
+pe_ratio = fetcher.fetch_metric(StockMetric.pe_ratio)
+
+# Fetch eb_to_ebidta
+ev_to_ebidta = fetcher.fetch_metric(StockMetric.ev_to_ebitda)
+
+# Fetch revenue
+revenue = fetcher.fetch_metric(StockMetric.revenue)
+
+# Fetch free cash flow
+fcf = fetcher.fetch_metric(StockMetric.free_cash_flow)
+
+x = 1
